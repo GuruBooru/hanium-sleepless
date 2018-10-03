07fb17b91afced11723049b52665a7643ae86f79
GuruBooru <123gudehsdl@naver.com>
2018-10-03 21:51:14 +0900

    테이블 체크박스 적용하기
    왜 체크박스가 보이지 않을까??

--- src/app/main-tab/tab-component/history-tab/history-tab.component.ts
+++ src/app/main-tab/tab-component/history-tab/history-tab.component.ts
@@ -1,23 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+import { UserService } from '../../../service/user.service';
+import { MatTableDataSource } from '@angular/material';
+import { SelectionModel } from '@angular/cdk/collections';
 
 export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  isFile: string;
+  context: string;
+  date: string;
+  result: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {isFile: '1', context: 'Hydrogen', date: '1.0079', result: 'H'},
+  {isFile: '2', context: 'Helium', date: '4.0026', result: 'He'},
+  {isFile: '3', context: 'Lithium', date: '6.941', result: 'Li'},
+  {isFile: '4', context: 'Beryllium', date: '9.0122', result: 'Be'},
+  {isFile: '5', context: 'Boron', date: '10.811', result: 'B'},
+  {isFile: '6', context: 'Carbon', date: '12.0107', result: 'C'},
+  {isFile: '7', context: 'Nitrogen', date: '14.0067', result: 'N'},
+  {isFile: '8', context: 'Oxygen', date: '15.9994', result: 'O'},
+  {isFile: '9', context: 'Fluorine', date: '18.9984', result: 'F'},
+  {isFile: '10', context: 'Neon', date: '20.1797', result: 'Ne'},
 ];
 
 /**
@@ -29,6 +34,32 @@ const ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: './history-tab.component.html',
 })
 export class HistoryTabComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['File/URL', 'Contexts', 'Date', 'Result'];
+  // dataSource = ELEMENT_DATA;
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  selection = new SelectionModel<PeriodicElement>(true, []);
+
+  historyTabForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private userService: UserService) {  }
+
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+
+    return numSelected === numRows;
+  }
+
+  masterToggle() {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  onSubmitHistory(fb: FormGroup) {
+    const id = sessionStorage.getItem('id');
+    // 세부 확인할 검사결과 모음
+
+    // 검사결과 전송
+  }
 }
--- src/app/main-tab/tab-component/history-tab/history-tab.module.ts
+++ src/app/main-tab/tab-component/history-tab/history-tab.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 import { HistoryTabComponent } from './history-tab.component';
 import { ResultModule } from './result/result.module';
+
 
 
 @NgModule({
@@ -14,6 +16,7 @@ import { ResultModule } from './result/result.module';
     ResultModule,
     ReactiveFormsModule,
     MatTableModule,
+    MatCheckboxModule,
   ],
   declarations: [HistoryTabComponent],
   exports: [HistoryTabComponent],
